85a72d604748d90f10f1d29b13110e000465de19
Adriano Miranda <adrjmiranda@gmail.com>
2024-04-04 15:00:03 -0300

    chore: added offer area

--- src/components/offer/index.tsx
+++ src/components/offer/index.tsx
@@ -2,9 +2,37 @@ import { FunctionComponent, ReactNode } from 'react';
 
 // Style
 import * as Style from './style';
+import { Wrapper } from '../../style/Components';
+
+// Image
+import HeroBg from '../../assets/img/hero-bg.jpg';
 
 const Offer: FunctionComponent = (): ReactNode => {
-	return <Style.Container>Offer</Style.Container>;
+	return (
+		<Style.Container
+			style={{
+				backgroundImage:
+					'linear-gradient(rgba(0, 0, 0, .3), rgba(0, 0, 0, .3)), url(' +
+					HeroBg +
+					')',
+			}}
+		>
+			<Style.Content>
+				<Wrapper>
+					<Style.Info>
+						<div className='offer-info'>
+							<h4>Get now 25% discount on the most visited destinations</h4>
+							<p>
+								Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet
+								saepe illum corrupti tempora a mollitia laborum sunt asperiores.
+							</p>
+						</div>
+						<button type='button'>Buy now</button>
+					</Style.Info>
+				</Wrapper>
+			</Style.Content>
+		</Style.Container>
+	);
 };
 
 export default Offer;
--- src/components/offer/style.ts
+++ src/components/offer/style.ts
@@ -1,3 +1,58 @@
 import styled from 'styled-components';
 
-export const Container = styled.div``;
+export const Container = styled.div`
+	background-position: center;
+	background-attachment: fixed;
+	background-repeat: no-repeat;
+	background-size: cover;
+	padding: 150px 0;
+`;
+
+export const Content = styled.div`
+	background-color: rgba(0, 0, 0, 0.8);
+	padding: 3rem 0;
+`;
+
+export const Info = styled.div`
+	display: flex;
+	align-items: center;
+	gap: 4rem;
+
+	.offer-info {
+		display: flex;
+		flex-direction: column;
+		gap: 1rem;
+		color: ${(props) => props.theme.color.neutralWhite};
+		flex: 50%;
+
+		h4 {
+			font-family: ${(props) => props.theme.fontFamily.merienda};
+			font-size: 1.8rem;
+			text-transform: capitalize;
+			color: inherit;
+		}
+
+		p {
+			color: inherit;
+			font-size: 1.1rem;
+		}
+	}
+
+	button {
+		flex: 50%;
+		cursor: pointer;
+		border: ${(props) => props.theme.borderWidht.bwSmall} solid inherit;
+		color: ${(props) => props.theme.color.neutralWhite};
+		padding: 1rem 2rem;
+		border-radius: ${(props) => props.theme.borderRadius.brLarge};
+		max-width: fit-content;
+		background-color: transparent;
+		text-transform: uppercase;
+		font-weight: ${(props) => props.theme.fontWeight.fwLarge};
+		transition: ${(props) => props.theme.transitioType.trMedium};
+
+		&:hover {
+			background-color: rgba(255, 255, 255, 0.1);
+		}
+	}
+`;
